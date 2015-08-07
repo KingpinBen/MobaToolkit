2267ff6f6e10cb32d6227611ac37817e88488caa
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:30:29 +0100

    New: Setup
    A necessary component to add to define the playspace so we can calculate
    the minimap space required and where there should be fog.

---
+++ Assets/Minimap/scripts/GameplayArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class GameplayArea : MonoBehaviour
+{
+    [SerializeField]
+    private float _size = 10.0f;
+
+    [SerializeField]
+    private Color _backgroundColor;
+
+    private void Awake()
+    {
+        Shader.SetGlobalFloat("_FogAreaSize", _size);
+        Shader.SetGlobalVector("_FogObjectWorldPosition", transform.position);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 pos = transform.position;
+
+        Gizmos.DrawWireCube(new Vector3(pos.x, 2.5f, pos.z), new Vector3(_size, 5, _size));
+    }
+
+    public float Size
+    {
+        get { return _size; }
+    }
+}
