0d288e502196758bd8934b336573e367aba0fe42
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:40:35 +0100

    New: Setup
    Provided a component to add so that players can look out but not in,
    much like the grass in League of Legends

---
+++ Assets/Fog Of War/GrassLoSBlock.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassLoSBlock : LineOfSightBlocker
+{
+    public const int cGrassSquareSize = 3;
+
+}
---
+++ Assets/Fog Of War/LineOfSightBlocker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class LineOfSightBlocker : MonoBehaviour
+{
+
+	void Start ()
+    {
+        var s = GetComponent<BoxCollider>();
+        s.isTrigger = true;
+	}
+
+    void OnDrawGizmos()
+    {
+        BoxCollider col = GetComponent<BoxCollider>();
+
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
+        Gizmos.color = Color.yellow * .5f;
+        Gizmos.DrawCube(col.center, col.size);
+    }
+}
