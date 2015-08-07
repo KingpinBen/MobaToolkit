fd19c89e912620fb7ccb319bff08d97b44ea7bf5
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:35:06 +0100

    New: Setup
    Component added to an object that should reveal the fog from the
    surrounding area. Should probably be used alongside minimaptrackable

---
+++ Assets/Fog Of War/FogOfWarRevealer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fog
+{
+    public sealed class FogOfWarRevealer : MonoBehaviour
+    {
+        [SerializeField]
+        private float _viewRadius = 10;
+
+        private Point _pointInFog;
+
+        void Start()
+        {
+            FogOfWarManager.Instance.AddTrackedRevealer(this);
+        }
+
+        public Point Point
+        {
+            get { return _pointInFog; }
+            set { _pointInFog = value; }
+            
+        }
+
+        public float Range
+        {
+            get { return _viewRadius; }
+        }
+    }
+}
+
