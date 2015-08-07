84a4b8ccdbb0c111473acffd35aafeb2a18b59bf
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:35:27 +0100

    New: Setup
    Asset for storing scene fog data

---
+++ Assets/Fog Of War/FogDataAsset.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public sealed class FogDataAsset : ScriptableObject
+{
+    [SerializeField]
+    private Fog.FogOfWarProbeData[] _probeData;
+
+    public Fog.FogOfWarProbeData[] Data
+    {
+        get { return _probeData; }
+    }
+
+    public int Count
+    {
+        get { return _probeData.Length; }
+    }
+
+#if UNITY_EDITOR
+    public void SetData(Fog.FogOfWarProbeData[] newData)
+    {
+        Debug.Assert(newData != null);
+
+        _probeData = newData;
+    }
+#endif
+}
---
+++ Assets/Fog Of War/FogOfWarProbeData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Fog
+{
+    [Serializable]
+    public sealed class FogOfWarProbeData
+    {
+        public int x;
+        public int z;
+
+        public int[] visibleProbesIndices;
+
+        [NonSerialized]
+        public bool isHidden = true;
+
+        public FogOfWarProbeData(int xx, int zz)
+        {
+            x = xx;
+            z = zz;
+        }
+
+        public FogOfWarProbeData(Point p)
+        {
+            x = p.x;
+            z = p.y;
+        }
+
+        public Point Point
+        {
+            get { return new Point(x, z); }
+        }
+    }
+}
