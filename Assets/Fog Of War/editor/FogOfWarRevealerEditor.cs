8a58ba61c0c3698258b9160bcc6c6e546ba69dcd
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:42:42 +0100

    New: Setup
    Basic editor to show in sceneview the area that is affected

---
+++ Assets/Fog Of War/editor/FogOfWarRevealerEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Fog;
+
+namespace Fog
+{
+    [CustomEditor(typeof(FogOfWarRevealer))]
+    public class FogOfWarRevealerEditor : Editor
+    {
+        private SerializedProperty _range;
+        private FogOfWarRevealer _target;
+
+    void OnEnable()
+        {
+            _target = target as FogOfWarRevealer;
+
+            _range = serializedObject.FindProperty("_viewRadius");
+        }
+
+        void OnSceneGUI()
+        { 
+            HelperMethods.DrawCircleWithOutline(_target.transform.position, Vector3.up, _range.floatValue, Color.white, Color.blue);
+        }
+    }
+
+}
