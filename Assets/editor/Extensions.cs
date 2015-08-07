4c048e30f28134afeced17e6331b4cf5f444807f
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:36:40 +0100

    New: Setup

---
+++ Assets/editor/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Extensions
+{
+    public static Vector2 Vector3ToVec2(this Vector3 vec3)
+    {
+        return new Vector2(vec3.x, vec3.z);
+    }
+}
