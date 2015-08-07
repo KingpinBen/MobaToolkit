84004e522ee19c296cb0c6be87a8b09805749a7e
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-07 02:29:27 +0100

    New: Setup
    Basic component to add itself to the minimap to be tracked

---
+++ Assets/Minimap/scripts/MinimapTrackable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Minimap
+{
+    public class MinimapTrackable : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite _sprite;
+
+        public Sprite Sprite
+        {
+            get { return _sprite; }
+        }
+
+        private void Start()
+        {
+            MinimapTracker.Instance.AddTrackable(this);
+        }
+    }
+}
