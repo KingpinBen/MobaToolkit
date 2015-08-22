9fb284770e52888dfa0c426e92e9a1d2024e2877
Ben Griffiths <scare_tfc@hotmail.com>
2015-08-22 13:03:20 +0100

    Added Ienumerator jobs for the editor to run
    Stops the editor stalling on big tasks

---
+++ Assets/editor/EditorJob.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public sealed class EditorJob
+{
+    private readonly IEnumerator _coroutine;
+    private bool _inProgress;
+
+    private EditorJob(IEnumerator coroutine)
+    {
+        _coroutine = coroutine; 
+        EditorApplication.update += Update;
+        _inProgress = true;
+    }
+
+    private void Stop()
+    {
+        EditorApplication.update -= Update;
+        _inProgress = false;
+    }
+
+    private void Update()
+    {
+        if (!_coroutine.MoveNext())
+            Stop();
+    }
+
+    public bool Finished
+    {
+        get { return !_inProgress; }
+    }
+
+    public static EditorJob Start(IEnumerator ienumerator)
+    {
+        return new EditorJob(ienumerator);
+    }
+}
